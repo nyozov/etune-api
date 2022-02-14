9566fa57935f319ebee9aebfd074783f0a2cf082
nikolay <n_yozov@hotmail.com>
2022-02-14 17:34:42 -0500

    add stripe post route

--- src/api.js
+++ src/api.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const serverless = require('serverless-http');
-
+require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 const router = express.Router();
 
@@ -11,6 +18,35 @@ router.get('/', (req, res) => {
   })
 })
 
+router.get('/payment', (req, res) => {
+  res.json({
+    "api": 'payment'
+  })
+})
+
+app.post("/payment", cors(), async (req, res) => {
+  let { amount, id } = req.body;
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "E-Tune Company",
+      payment_method: id,
+      confirm: true,
+    });
+    console.log("payment", payment.status);
+    res.json({
+      message: "Payment Successful",
+      success: true,
+    });
+  } catch (error) {
+    console.log("error", error);
+    res.json({
+      message: "Payment Failed",
+      success: false,
+    });
+  }
+});
 app.use('/.netlify/functions/api', router);
 
 module.exports.handler = serverless(app)
